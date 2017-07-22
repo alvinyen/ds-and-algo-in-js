fc48b60873c0d504fee6bdaec9813ae16d2592e0
yamapi06150 <yamapi06150@gmail.com>
2017-07-22 19:46:20 +0800

    complete the basic ebvironment setting

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+const server = http.createServer(app);
+server.listen(8080, () => {
+    console.log('server is running on 8080');
+});
