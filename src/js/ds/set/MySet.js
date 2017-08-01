41d7415882c10320c9dd34ee22c375d6165acf91
alvinyen <103356015@nccu.edu.tw>
2017-08-01 23:56:52 +0800

    add values, has and add functionality to mySet

---
+++ src/js/ds/set/MySet.js
@@ -0,0 +1,23 @@
+function mySet() {
+  // the let collection will hold the set
+  let collection = [];
+
+  // this method will check for the presence of an element and return true or false
+  this.has = function (element) {
+    return (collection.indexOf(element) !== -1);
+  }; // note logic、Array.indexOf
+
+  // this method will return all the values in the set
+  this.values = function () {
+    return collection;
+  };
+
+  // this method will add an element to the set
+  this.add = function (element) {
+    if (!this.has(element)) {
+      collection.push(element);
+      return true;
+    }
+    return false;
+  };
+}
